542d17d02dea2cc415bd1a8255a34987edc42da6
John <blackjlc@gmail.com>
2021-01-28 02:14:43 +0000

    Implemented animation for walking and idle. Tested sprite rigging

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Assertions;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,8 +11,14 @@ public class PlayerController : MonoBehaviour
     public InputAction controlMap;
     public float speed;
     public float interactRange;
-
     public LayerMask interactableLayer;
+    public Animator animator;
+    private int moveAniID;
+    private int moveXAniID;
+    private int moveSpeedAniID;
+    private int carryAniID;
+    private int pickKeyAniID;
+    private int danceAniID;
 
     private CharacterController controller;
     private new Transform transform;
@@ -35,6 +42,13 @@ public class PlayerController : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         transform = GetComponent<Transform>();
+        Assert.IsNotNull(animator, "Animator should not be null!!");
+        moveAniID = Animator.StringToHash("move");
+        moveXAniID = Animator.StringToHash("move_x");
+        moveSpeedAniID = Animator.StringToHash("move_speed");
+        carryAniID = Animator.StringToHash("carry");
+        pickKeyAniID = Animator.StringToHash("pick_key");
+        danceAniID = Animator.StringToHash("dance");
     }
 
     private void Update()
@@ -42,13 +56,22 @@ public class PlayerController : MonoBehaviour
         Vector2 inputVector = controlMap.ReadValue<Vector2>();
         Vector3 finalVector = new Vector3(inputVector.x, 0, inputVector.y);
 
+        // print(finalVector);
+        HandleAnimation(finalVector.x, finalVector.sqrMagnitude);
+
         //Debug.Log(finalVector.ToString());
         controller.Move(finalVector * Time.deltaTime * speed);
     }
 
     private void HandleInteract(InputAction.CallbackContext obj)
     {
-        Collider[] interactables = Physics.OverlapSphere(transform.position, 1, interactableLayer);
+        Collider[] interactables = Physics.OverlapSphere(transform.position, interactRange, interactableLayer);
         interactables[0].GetComponent<IInteractable>().Interact();
+    }
+
+    private void HandleAnimation(float speedX, float speed)
+    {
+        animator.SetFloat(moveXAniID, speedX > 0 ? 1f : -1f);
+        animator.SetFloat(moveSpeedAniID, speed > 0 ? 1f : 0f);
     }
 }
