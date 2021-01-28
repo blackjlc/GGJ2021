291cc2a4e5be6b712f9ce41bd92bfc85a6290ae3
cnlauab <francis3245000@hotmail.com>
2021-01-28 17:46:41 +0800

    Implemented Wandering NPC

---
+++ Assets/Scripts/WanderMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderMovement : MonoBehaviour
+{
+    public float speed;
+    public float range;
+    public Vector3 origin;
+    public float radius;
+
+    Vector3 wayPoint;
+
+    private bool IsInsideBoundary(Vector3 point) {
+        return Vector3.Distance(point, origin) < radius;
+    }
+
+    private void SetDestination() {
+        Vector3 destination = new Vector3();
+        do {
+            destination.x = transform.position.x + Random.Range(-range, range);
+            destination.y = transform.position.y;
+            destination.z = transform.position.z + Random.Range(-range, range);
+        } while (!IsInsideBoundary(destination));
+
+        wayPoint = destination;
+    }
+
+
+    void Start()
+    {
+        SetDestination();
+    }
+
+    
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, wayPoint, speed * Time.deltaTime);
+        if(Vector3.Distance(transform.position, wayPoint) < 0.1f) {
+            SetDestination();
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(origin, radius);
+    }
+}
