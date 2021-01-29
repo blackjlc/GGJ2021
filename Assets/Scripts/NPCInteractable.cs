b16d5a642fbd1be0d091d5779d3c1a61ab2c99b7
John <blackjlc@gmail.com>
2021-01-29 21:12:13 +0000

    Updated carry and uncarry behavour

--- Assets/Scripts/NPCInteractable.cs
+++ Assets/Scripts/NPCInteractable.cs
@@ -2,37 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCInteractable : MonoBehaviour,IInteractable
+public class NPCInteractable : MonoBehaviour, IInteractable
 {
     public string npcName;
     public GameObject textBubblePrefab;
     public Vector3 textBubbleOffset;
     public bool isFriend;
 
-    public bool CanInteract() {
+    public bool CanInteract()
+    {
         return true;
     }
 
-    public GameObject Interact(PlayerData playerData) {
+    public GameObject Interact(PlayerData playerData)
+    {
         Debug.Log(gameObject.name + " says Hi.");
         Debug.Log("Carrying " + playerData.carryingFriend);
-        if (!isFriend) {
-            if(transform.Find("TextBubble(Clone)") != null) {
+        if (!isFriend)
+        {
+            if (transform.Find("TextBubble(Clone)") != null)
+            {
                 Destroy(transform.Find("TextBubble(Clone)").gameObject);
             }
             GameObject go = Instantiate(textBubblePrefab, transform.position + textBubbleOffset, Quaternion.identity, transform);
             go.GetComponent<TextBubble>().Setup(gameObject.name + " says Hi.");
             Destroy(go, 6f);
             return gameObject;
-        } else if(playerData.carryingFriend == null || playerData.carryingFriend.Length <= 0 ) {
+        }
+        else if (playerData.carryingFriend == null || playerData.carryingFriend.Length <= 0)
+        {
             playerData.carryingFriend = npcName;
+            playerData.animation.SetCarry(true);
             gameObject.SetActive(false);
             return null;
         }
         return gameObject;
     }
 
-    public void ToggleHighlight() {
+    public void ToggleHighlight()
+    {
         //TODO
         Debug.Log("Toggle Highlight for " + gameObject.name);
     }
