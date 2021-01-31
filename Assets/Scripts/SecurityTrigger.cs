3aec6549b8c123a3dd456da14884a635cbad5dc3
cnlauab <francis3245000@hotmail.com>
2021-01-31 08:18:24 +0800

    Security Trigger

--- Assets/Scripts/SecurityTrigger.cs
+++ Assets/Scripts/SecurityTrigger.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class SecurityTrigger : MonoBehaviour
 {
     public float detectRange;
+    public LayerMask playerLayer;
+    private GameManager gm;
 
+    private void Awake() {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
 
+    void Update() {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, detectRange, playerLayer);
+        if(colliders.Length > 0) {
+            gm.TriggerAllSecurity();
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(transform.position, detectRange);
+    }
 }
